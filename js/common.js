69bffc3ce9cfd487d1de7759b7729ab673113d0e
cx4 <cybors@yandex.com>
2019-04-24 17:09:16 +0800

    change javascript content
    change javascript content

--- js/common.js
+++ js/common.js
@@ -1,6 +1,7 @@
 <!--Add the following script at the bottom of the web page (before </body></html>)-->
 <script type="text/javascript">function add_chatinline(){var hccid=29199833;var nt=document.createElement("script");nt.async=true;nt.src="https://mylivechat.com/chatinline.aspx?hccid="+hccid;var ct=document.getElementsByTagName("script")[0];ct.parentNode.insertBefore(nt,ct);}
 add_chatinline();</script>
+
 $(document).ready(function () {
     $('.douban_item').each(function () {
         var id = $(this).attr('date-dbid').toString();
